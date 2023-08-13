3f2944f70b64300f65e498dbff164d5e5b8d3a8d
Mally01 <>
2023-08-13 16:47:55 +0530

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { AboutComponent } from './about/about.component';
+import { StatsComponent } from './stats/stats.component';
+import { ServicesComponent } from './services/services.component';
+import { ClientsComponent } from './clients/clients.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FeaturedComponent,
+    AboutComponent,
+    StatsComponent,
+    ServicesComponent,
+    ClientsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
